ac767b10e541b925305e65f7d3334943dd6c6212
Raphi04 <raphaelcadete04@gmail.com>
2023-11-01 20:59:19 +0100

    J'ai régler mon problème yes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var musicState = "off";
+var firstimeMusic = "true"
+
 function addHTML(divID, documentHTML) {
     document.getElementById(divID).innerHTML = `<object type="text/html" data="${documentHTML}"></object>`;
 }
@@ -8,4 +11,24 @@ function changeWindow(newWindowID){
     let newWindow = document.getElementById(newWindowID);
     allWindows.forEach(window => window.classList.add("windowDisable"));
     newWindow.classList.remove("windowDisable");
+}
+
+function playMusic() {
+    if (firstimeMusic == "true") {
+        document.getElementById("music").play();
+        document.getElementById("sound").src="ressources/main/soundOn.png";
+        musicState = "on";
+        firstimeMusic = "false"
+    }
+}
+function musicStatement() {
+    if (musicState == "off") {
+        document.getElementById("music").play();
+        document.getElementById("sound").src="ressources/main/soundOn.png";
+        musicState = "on";
+    } else {
+        document.getElementById("music").pause()
+        document.getElementById("sound").src="ressources/main/soundOff.png";
+        musicState = "off";
+    }
 }
